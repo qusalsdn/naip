fc15b228a2f7659b10699bf530b9a01dc2a22aff
변민우 <yja05092@gmail.com>
2024-02-21 16:06:10 +0900

    DynamicServerError Bug0

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,22 +1,18 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 const createConnection = require("../../../src/db");
 
 export async function GET(req: NextRequest) {
   const connection = await createConnection();
   try {
-    const cookie = cookies().getAll();
-    if (cookie.length > 0) {
-      const token = cookie[0].value;
-      const [results] = await connection.execute(`select * from userInfo where token='${token}'`);
-      if (results.length !== 0) {
-        await connection.end();
-        return NextResponse.json({ ok: true, user: results[0] });
-      } else {
-        await connection.end();
-        return NextResponse.json({ ok: false });
-      }
+    const token = cookies().get("token")?.value;
+    const [results] = await connection.execute(`select * from userInfo where token='${token}'`);
+    if (results.length !== 0) {
+      await connection.end();
+      return NextResponse.json({ ok: true, user: results[0] });
     } else {
       await connection.end();
       return NextResponse.json({ ok: false });
